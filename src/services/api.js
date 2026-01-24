2831c9ac14ea35d49ea6a3a790441ad494be2eb1
Israel <isekundayo700@gmail.com>
2026-01-24 16:03:04 +0100

    Fix production API baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.PROD
-    ? "https://portify-backend-hm08.onrender.com"
+    ? "https://portify-backend-hm08.onrender.com/api"
     : "/api",
 });
 
